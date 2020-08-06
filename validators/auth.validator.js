a88fd55bf2ae7e6bba0721cf1e1775ee0c9adc2f
Hitz Huang <hitz@pjx-studios.com>
2020-08-07 03:54:17 +0800

    fixed register input validation.

--- validators/auth.validator.js
+++ validators/auth.validator.js
@@ -3,13 +3,13 @@ const { check } = require("express-validator");
 exports.register = [
     check("email", "Email is required.").normalizeEmail().isEmail(),
     check("username", "Username is required.").notEmpty(),
-    check("password", "Password is required.").exists(),
-    check("passwordConfirmation")
-        .exists()
+    check("password", "Password is required.").notEmpty(),
+    check("confirmPassword", "Password is not the same.")
+        .notEmpty()
         .custom((value, { req }) => value === req.body.password),
 ];
 
 exports.login = [
     check("email", "Email is required.").normalizeEmail().isEmail(),
-    check("password", "Password is required.").exists(),
+    check("password", "Password is required.").notEmpty(),
 ];
